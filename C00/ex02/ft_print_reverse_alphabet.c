058a1b2960adc736780f785d0a34ce0d9136eb85
Zoubair Entifi <zentifi@e2r13p1.1337.ma>
2023-10-01 15:52:49 +0100

    alo

---
+++ C00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zentifi <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/17 23:19:47 by zentifi           #+#    #+#             */
+/*   Updated: 2023/09/17 23:21:45 by zentifi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	alphabet;
+
+	alphabet = 'z';
+	while (alphabet >= 'a')
+	{
+		write (1, &alphabet, 1);
+		alphabet--;
+	}
+}
